dd9c8aeff744cbace19d110e8e75d32540cff7d1
edgardfn <edgardfinotti@yahoo.com.br>
2022-11-07 01:43:38 -0300

    terminado intens do header e do menu e feito parte de exibir um submenu ao clicar na seta para baixo.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 console.log('teste')
+
+function clickSites () {
+    var icon = document.getElementById('icon-sites');
+    var div = document.getElementById('container-submenu');
+    var styles = window.getComputedStyle(div);
+    console.log('display:', styles.display);
+    if(styles.display === 'none') {
+        div.style.display = 'block';
+        icon.classList.remove('fa-caret-down');
+        icon.classList.add('fa-caret-up');
+    } else {
+        div.style.display = 'none';
+        icon.classList.remove('fa-caret-up');
+        icon.classList.add('fa-caret-down');
+    }
+}
